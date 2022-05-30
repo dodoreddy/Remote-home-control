07be443296a1f52bf5302327f4f5975094b6d656
dodoreddy <samrudr@gmail.com>
2022-05-30 15:23:39 +0530

    Starter hello world

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hi");
+express = require('express')
+app = express()
+
+port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {console.log('listening on port ' + port)})
