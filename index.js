8d2cd3132dead37a5b5592b861b224704d297dfb
dodoreddy <samrudr@gmail.com>
2022-05-31 14:00:33 +0530

    Added some ses storage

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
+//imports
 const express = require('express');
 const path = require('path');
 const socket = require('socket.io') 
-
+//port and express init
 const app = express();
 const port = process.env.PORT || 3000;
 
+//allows acess to landing page
 app.use('/', express.static('public/landing_page'));
 
-// sendFile will go here
+//lanidng page start
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/landing_page/index.html'));
 });
 
+//allows acsess to home page
 app.use('/home', express.static('public/home'));
 
+//home page start
 app.get('/home', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/home/index.html'));
 });
 
+//starts server
 var server = app.listen(port);
 console.log('Server started at http://localhost:' + port);
 
+//io start
 io = socket(server)
 
+//socket connection
 io.on('connection', function(socket){
   console.log("Made a connection:- "+socket.id)
 
+  io.to(socket.id).emit('id', socket.id)
+  
   socket.on('pwd', function(data){
     console.log(data)
   })
--- public/landing_page/script.js
+++ public/landing_page/script.js
@@ -1,4 +1,6 @@
-socket = io.connect("https://test-website.dodo135.repl.co")
+socket = io.connect("http://localhost:3000")
+
+sessionStorage.clear()
 
 button = document.getElementById("btn");
 pwd = document.getElementById("pasword")
@@ -6,4 +8,13 @@ console.log(document.getElementById('btn'))
 
 button.addEventListener('click', function() {
   socket.emit('pwd', pwd.value);
+  sessionStorage.setItem("pwd", pwd.value);
+
+  window.location.href = ("http://localhost:3000/home");
+})
+
+
+socket.on('id', function(data){
+  console.log(data);
+  sessionStorage.setItem("id", data)
 })
